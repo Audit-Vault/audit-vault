a6fdd0e5532d0e2eca95f3f7f875dd58f21c8414
Hairum Qureshi <hqureshi@udel.edu>
2026-02-28 15:40:38 -0500

    added code for logging the decoded user token from Firebase for signInWithGoogle function endpoint

--- backend/controllers/authentication.ts
+++ backend/controllers/authentication.ts
@@ -1,7 +1,19 @@
 import { Request, Response } from "express";
+import admin from "../config/firebase";
 
 const signInWithGoogle = async (req: Request, res: Response) => {
 	try {
+		const authHeader = req.headers.authorization;
+
+		if (authHeader) {
+			const decodedToken = await admin
+				.auth()
+				.verifyIdToken(authHeader.replace("Bearer ", ""));
+
+			console.log(decodedToken);
+		} else {
+			return res.status(401).json({ message: "No token provided" });
+		}
 	} catch (error) {
 		console.error(
 			"Error during Google sign-in inside of signInWithGoogle function:",
