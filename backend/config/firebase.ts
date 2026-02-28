4fe6b03c442c22be2c433a4bd4af38d6fe909ae1
Hairum Qureshi <hqureshi@udel.edu>
2026-02-28 15:10:04 -0500

    imported dotenv for service account to be recognized and imported properly from ENV file

--- backend/config/firebase.ts
+++ backend/config/firebase.ts
@@ -1,9 +1,14 @@
 import admin, { ServiceAccount } from "firebase-admin";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const serviceAccountJson = process.env.FIREBASE_SERVICE_ACCOUNT;
+
+const serviceAccount = JSON.parse(serviceAccountJson!) as ServiceAccount;
 
 admin.initializeApp({
-	credential: admin.credential.cert(
-		JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT!) as ServiceAccount
-	)
+	credential: admin.credential.cert(serviceAccount)
 });
 
 export default admin;
