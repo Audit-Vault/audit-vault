f106790fb59b089d4d2f0442b8a09780265d2498
Hairum Qureshi <hqureshi@udel.edu>
2026-02-28 14:54:28 -0500

    added CORS config and added middleware for /api/auth endpoints

--- backend/server.ts
+++ backend/server.ts
@@ -2,6 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import colors from "colors";
+import authRoutes from "./routes/authentication";
+import cors from "cors";
 
 dotenv.config();
 colors.enable();
@@ -10,6 +12,20 @@ const app = express();
 
 const MONGO_URI: string = process.env.MONGO_URI!;
 const PORT: string | number = process.env.PORT! || 3000;
+
+const corsOptions = {
+	origin: process.env.FRONTEND_URL, // <-- if your frontend port is different, change it
+	credentials: true,
+	optionSuccessStatus: 200
+};
+
+// Middleware:
+app.use(cors(corsOptions)); // <-- for CORS
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
 	res.send("Hello, World!");
