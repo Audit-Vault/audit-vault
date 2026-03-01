52f88a408be4c9ccecbfa711fdc1de5e2f79929c
Hairum Qureshi <hqureshi@udel.edu>
2026-02-28 20:56:48 -0500

    created types file

---
+++ backend/types.ts
@@ -0,0 +1,41 @@
+type User = {
+	_id: string;
+	picture: string;
+	email: string;
+	name: string;
+};
+
+type Instruction = {
+	type: string;
+	description: string;
+	data: any;
+	status: "pending" | "completed" | "failed";
+	createdAt: Date;
+	completedAt: Date;
+	result: any;
+	error: string;
+};
+
+type Vulnerability = {
+	name: string;
+	description: string;
+	severity: string;
+	data: any; // includes logs and permissions related to it
+};
+
+type Scan = {
+	date: Date;
+	filePermissions: any;
+	logs: any;
+	users: any;
+};
+
+type Server = {
+	name: string;
+	uuid: string;
+	instructions: Instruction[];
+	vulnerabilities: Vulnerability[];
+	scans: Scan[];
+};
+
+export { User, Instruction, Vulnerability, Scan, Server };
