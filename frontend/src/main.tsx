7ea0516bdaa11d85ce2402beac6e8a1e5754badf
Hairum Qureshi <hqureshi@udel.edu>
2026-02-28 19:09:52 -0500

    configured react-query and enabled React Query Devtools

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,14 @@
+import { createRoot } from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"; // 1. Import it
+import App from "./App.tsx";
+import "./index.css";
 
-  import { createRoot } from "react-dom/client";
-  import App from "./App.tsx";
-  import "./index.css";
+const queryClient = new QueryClient();
 
-  createRoot(document.getElementById("root")!).render(<App />);
-  
+createRoot(document.getElementById("root")!).render(
+	<QueryClientProvider client={queryClient}>
+		<App />
+		<ReactQueryDevtools initialIsOpen={false} />
+	</QueryClientProvider>
+);
