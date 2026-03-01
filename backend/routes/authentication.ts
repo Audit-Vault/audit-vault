88ba34d03c672573afdf32130dd13f99ed8bee6f
Hairum Qureshi <hqureshi@udel.edu>
2026-02-28 21:22:08 -0500

    added custom 'isAuthenticated' middleware to GET current user endpoint

--- backend/routes/authentication.ts
+++ backend/routes/authentication.ts
@@ -1,9 +1,13 @@
 import express from "express";
-import { getCurrentUser, signInWithGoogle } from "../controllers/authentication";
+import {
+	getCurrentUser,
+	signInWithGoogle
+} from "../controllers/authentication";
+import { isAuthenticated } from "../middleware/isAuthenticated";
 
 const router = express.Router();
 
 router.post("/google/sign-in", signInWithGoogle);
-router.get("/current-user", getCurrentUser);
+router.get("/current-user", isAuthenticated, getCurrentUser);
 
 export default router;
